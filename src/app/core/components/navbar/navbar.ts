98200615e3d39855c2d7e54ac899d474a86d8f87
Rogadev12 <gabrielomarrolon1996@gmail.com>
2026-06-12 19:38:33 -0300

    correciones

--- src/app/core/components/navbar/navbar.ts
+++ src/app/core/components/navbar/navbar.ts
@@ -1,4 +1,5 @@
-import { Component, signal, HostListener } from '@angular/core';
+import { Component, signal, HostListener, effect, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -10,6 +11,7 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  private platformId = inject(PLATFORM_ID);
   protected readonly menuOpen = signal(false);
   protected readonly avatarOpen = signal(false);
   protected readonly hidden = signal(false);
@@ -29,7 +31,12 @@ export class Navbar {
     { path: '/participacion', label: 'Tu Barrio Decide' },
   ];
 
-  constructor(protected auth: AuthService) {}
+  constructor(protected auth: AuthService) {
+    effect(() => {
+      if (!isPlatformBrowser(this.platformId)) return;
+      document.body.style.overflow = this.menuOpen() ? 'hidden' : '';
+    });
+  }
 
   @HostListener('window:scroll')
   onScroll(): void {
